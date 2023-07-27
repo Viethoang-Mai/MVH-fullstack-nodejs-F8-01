8b980575b91a5def1e9d22e521608fe5c9d71063
ViethoangMai-dev <viethoangmai21199@.com>
2023-07-27 10:36:00 +0700

    Buoi hoc 16

---
+++ Hoc_Js/hello_world/variables/js/script.js
@@ -0,0 +1,12 @@
+// Khai bao bien
+
+// var customerName; /*camelCase đặt tên biến, tên hàm*/
+var userName = "Mai Viet Hoang";
+console.log(userName);
+document.write(userName);
+var company = "F8";
+var welcome =
+    "<h2> Chao mung ban den " + company + ' toi la "Mai Viet Hoang"</h2>';
+// var welcome = `<h2> Chao mung ban den ` + company + `</h2>`;
+console.log(welcome);
+document.write(welcome);
---
+++ Hoc_Js/operator/js/ex.js
@@ -0,0 +1,4 @@
+var a = 33;
+var b = a ? "Đ" : "S";
+
+console.log(b);
